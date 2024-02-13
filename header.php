e30dc0749a26a1e520f9eb11b7752c8a7af8454e
Quentin <quentindev02@gmail.com>
2024-02-13 12:26:48 +0100

    Premier design header

--- header.php
+++ header.php
@@ -3,33 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="src\styles\style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="src/styles/style.css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
     <header>
-        <div>
-            <h1>Garage Vincent Parrot</h1>
-            <img src="" alt="logo Garage VP">
-        </div> 
-        <nav>
+        <div class="logo-container">
+            <a href="index.php">
+             <img src="src/img/logo.png" alt="logo Garage VP">
+            </a>
+            
+        </div>
+        <nav class="main-nav">
             <ul>
-                <li><a href="index.php">Accueil</a></li>
-                <li><a href="services.php">Services</a></li>
-                <li><a href="contact.php">Contact</a></li>
-                <?php
-                // Vérifier si l'utilisateur est connecté
-                if ($utilisateurConnecte) {
-                    echo '<li><a href="profil.php">Profil</a></li>';
-                    echo '<li><a href="deconnexion.php">Déconnexion</a></li>';
-                } else {
-                    echo '<li><a href="connexion.php">Connexion</a></li>';
-                }
-                ?>
-                <li><a href="voitures_occasion.php">Voitures d'occasion</a></li>
-                <li><a href="services.php">Services</a></li>
+                <li><a href="#" class="nav-link">Accueil</a></li> <!-- Ajout de liens valides ou des placeholders pour les liens -->
+                <li><a href="#" class="nav-link">Horaires</a></li>
+                <li><a href="#" class="nav-link">Services</a></li>
+                <li><a href="#" class="nav-link">Voitures</a></li>
+                <li><a href="#" class="nav-link">Contact</a></li>
             </ul>
         </nav>
+        <div class="espace-pro">
+            <a href="#" class="btn-espace-pro">Espaces Professionnel</a> <!-- Ajout de liens valides ou des placeholders -->
+        </div>
     </header>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Votre Site Web - Accueil</title>
     <!-- Inclure le header -->
-    <?php include('header.php'); ?>
+    <?php include("header.php"); ?>
 </head>
 <body>
 
